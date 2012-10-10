f3d86b7ee30b5583ddaf6ea4e264d405472cdb92
Daniel Wallace <daniel.wallace12@gmail.com>
2012-10-10 18:02:57 -0400

    Add License

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
+#  This file is part of telnetecho.
+#
+#  Copyright 2012 Daniel Wallace
+#
+#  telnetecho is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
 PREFIX?=/usr
 CFLAGS=-Wall -g -DNEBUG -I$(PREFIX)/include/systemd
 LDFLAGS=-L$(PREFIX)/include/systemd
--- buffers.h
+++ buffers.h
@@ -1,3 +1,21 @@
+/***
+  This file is part of telnetecho.
+
+  Copyright 2012 Daniel Wallace
+
+  telnetecho is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  telnetecho is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with telnetecho; If not, see <http://www.gnu.org/licenses/>.
+***/
 #ifndef _telnet_echo
 #define _telnet_echo
 
